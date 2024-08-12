5251a379c1b04e66e924d48756f3a2d0b81ae106
A.S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-08-12 16:57:55 +0530

    Update app.js
    snvs

--- app.js
+++ app.js
@@ -1,123 +1,73 @@
-const express = require('express')
-const path = require('path')
-const {open} = require('sqlite')
-const sqlite3 = require('sqlite3')
+const express = require('express');
+const path = require('path');
+const { open } = require('sqlite');
+const sqlite3 = require('sqlite3');
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const dbPath = path.join(__dirname, 'covid19India.db')
-
-let db = null
+const dbPath = path.join(__dirname, 'covid19India.db');
+let db = null;
 
 const initializationDbserver = async () => {
   try {
     db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
-    })
+    });
     app.listen(3000, () => {
-      console.log('The server is running at http://localhost:3000')
-    })
+      console.log('The server is running at http://localhost:3000');
+    });
   } catch (e) {
-    console.log(`DB Server Error: ${e.message}`)
-    process.exit(1)
+    console.log(`DB Server Error: ${e.message}`);
+    process.exit(1);
   }
-}
+};
 
-initializationDbserver()
+initializationDbserver();
 
 // Convert snake_case to camelCase
-const toCamelCase = snakeCase => {
-  const camelCaseObj = {}
+const toCamelCase = (snakeCase) => {
+  const camelCaseObj = {};
   for (const key in snakeCase) {
     if (snakeCase.hasOwnProperty(key)) {
       const camelCaseKey = key.replace(/_([a-z])/g, (match, letter) =>
-        letter.toUpperCase(),
-      )
-      camelCaseObj[camelCaseKey] = snakeCase[key]
+        letter.toUpperCase()
+      );
+      camelCaseObj[camelCaseKey] = snakeCase[key];
     }
   }
-  return camelCaseObj
-}
+  return camelCaseObj;
+};
 
 // Returns a list of all states in the state table
 app.get('/states/', async (request, response) => {
-  const getStates = `SELECT * FROM state`
-  const theData = await db.all(getStates)
-  response.send(theData.map(toCamelCase)) //  send method
-})
+  const getStates = `SELECT * FROM state`;
+  const theData = await db.all(getStates);
+  response.send(theData.map(toCamelCase));
+});
 
 // Returns a state based on the state ID
 app.get('/states/:stateId/', async (request, response) => {
-  const {stateId} = request.params
-
+  const { stateId } = request.params;
   const getState = `
-  SELECT * 
-  FROM state 
-  WHERE state_id = ${stateId};`
+    SELECT * 
+    FROM state 
+    WHERE state_id = ${stateId};
+  `;
+  const fetchData = await db.get(getState);
+  response.send(toCamelCase(fetchData));
+});
 
-  const fetchData = await db.get(getState)
-  const camelCase = toCamelCase(fetchData) // Corrected: call toCamelCase directly
-  response.send(camelCase) //  send method
-})
-
-app.post('/districts/', async (request, response) => {
-  const theDistrict = request.body
-  const {districtName, stateId, cases, cured, active, deaths} = theDistrict
-
-  const addTheData = ` INSERT INTO 
-  district(district_name,state_id,cases,curved,active,deaths)
-  VALUES(
-    ${districtName},
-    ${stateId},
-    ${cases},
-    ${cured},
-    ${active},
-    ${deaths}
-  )
-  `
-  const dbResponse = await db.run(addTheData)
-  response.send('District Successfully Added')
-})
-
-app.get('/districts/:districtId/', async (request, response) => {
-  const {districtId} = request.params
-  const theData = `SELECT 
-  * 
-  FROM 
-  district
-  WHERE
-  district_id = ${districtId};`
-
-  const fetchData = await db.get(theData)
-  const camelCase = toCamelCase(fetchData)
-  response.send(camelCase)
-})
-
-//delete the data
-app.delete('/districts/:districtId/', async (request, response) => {
-  const {districtId} = request.params
-  const deleteData = `
-  DELETE
-  FROM 
-  district
-  WHERE
-  district_id = ${districtId};`
-
-  await db.run(deleteData)
-  response.send('District Removed')
-})
-///update the data
+// Update district details
 app.put('/districts/:districtId/', async (request, response) => {
-  const {districtId} = request.params;
-  const theDetails = request.body;
-  const { districtName, stateId, cases, cured, active, deaths } = theDetails;
+  const { districtId } = request.params;
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
 
   const updateData = `
     UPDATE district
     SET
-      district_name = ${districtName},
+      district_name = '${districtName}',
       state_id = ${stateId},
       cases = ${cases},
       cured = ${cured},
@@ -131,46 +81,78 @@ app.put('/districts/:districtId/', async (request, response) => {
   response.send('District Details Updated');
 });
 
-//get the data in a counted way
-
+// Get statistics of a specific state
 app.get('/states/:stateId/stats/', async (request, response) => {
-  const {stateId} = request.params
+  const { stateId } = request.params;
 
-  const getState = `
-  SELECT 
-    SUM(cases) AS totalCases,
-    SUM(cured) AS totalCured,
-    SUM(active) AS totalActive,
-    SUM(deaths) AS totalDeath
-  FROM district
-  WHERE state_id = ${stateId};`
+  const getStateStatsQuery = `
+    SELECT 
+      SUM(cases) AS totalCases,
+      SUM(cured) AS totalCured,
+      SUM(active) AS totalActive,
+      SUM(deaths) AS totalDeaths
+    FROM district
+    WHERE state_id = ${stateId};
+  `;
 
-  const stats = await db.get(getState)
-  console.log(stats)
+  const stats = await db.get(getStateStatsQuery);
+  response.send(toCamelCase(stats));
+});
 
-  response.send({
-    totalCases: stats['totalCases'],
-    totalCured: stats['totalCured'],
-    totalActive: stats['totalActive'],
-    totalDeath: stats['totalDeath'],
-  })
-})
-
-///get form the two tables
+// Get state name from district ID
 app.get('/districts/:districtId/details/', async (request, response) => {
-  const {districtId} = request.params
+  const { districtId } = request.params;
   const getDistrictIdQuery = `
-    select state_id from district
-    where district_id = ${districtId};
-    `
-  //With this we will get the state_id using district table
-  const getDistrictIdQueryResponse = await database.get(getDistrictIdQuery)
-  const getStateNameQuery = `
-    select state_name as stateName from state
-    where state_id = ${getDistrictIdQueryResponse.state_id};
-    ` //With this we will get state_name as stateName using the state_id
-  const getStateNameQueryResponse = await database.get(getStateNameQuery)
-  response.send(getStateNameQueryResponse)
-}) //sending the required response
+    SELECT state_id FROM district 
+    WHERE district_id = ${districtId};
+  `;
+  const getDistrictIdQueryResponse = await db.get(getDistrictIdQuery);
 
-module.exports = app
+  const getStateNameQuery = `
+    SELECT state_name AS stateName 
+    FROM state 
+    WHERE state_id = ${getDistrictIdQueryResponse.state_id};
+  `;
+  const getStateNameQueryResponse = await db.get(getStateNameQuery);
+  response.send(getStateNameQueryResponse);
+});
+
+// Delete a district
+app.delete('/districts/:districtId/', async (request, response) => {
+  const { districtId } = request.params;
+  const deleteData = `
+    DELETE
+    FROM district
+    WHERE district_id = ${districtId};
+  `;
+
+  await db.run(deleteData);
+  response.send('District Removed');
+});
+
+// Add a new district
+app.post('/districts/', async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+
+  const addDistrictQuery = `
+    INSERT INTO district (district_name, state_id, cases, cured, active, deaths)
+    VALUES ('${districtName}', ${stateId}, ${cases}, ${cured}, ${active}, ${deaths});
+  `;
+
+  await db.run(addDistrictQuery);
+  response.send('District Successfully Added');
+});
+
+// Get district details
+app.get('/districts/:districtId/', async (request, response) => {
+  const { districtId } = request.params;
+  const getDistrictQuery = `
+    SELECT * 
+    FROM district 
+    WHERE district_id = ${districtId};
+  `;
+  const district = await db.get(getDistrictQuery);
+  response.send(toCamelCase(district));
+});
+
+module.exports = app;
